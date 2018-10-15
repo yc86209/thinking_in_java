1885c0a3b2dae88476f0e7856cfe0e265b700850
zkrlibin <li_bin@sino.com.cn>
2018-10-15 17:55:48 +0800

    第五章终结

---
+++ 05_init_and_clear/src/main/java/Args.java
@@ -0,0 +1,17 @@
+/**
+ * @author: li_bin .
+ * @description: 可变参数列表 .
+ * @create: 2018-10-15 17:18 .
+ */
+public class Args {
+    public static void main(String[] args) {
+        /*   可变参数列表的长度可以为0   */
+//        new Args().f();
+    }
+    void f(Integer ... i){
+
+    }
+    void f(Long ... i){
+
+    }
+}
